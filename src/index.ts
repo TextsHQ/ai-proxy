7383bef3387b3951922116ff26ebe46beeee3a97
batuhan içöz <batuhan@icoz.co>
2023-12-02 15:27:20 +0100

    init openai proxy

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,87 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import * as z from 'zod';
+import jwt from '@tsndr/cloudflare-worker-jwt';
 
-import handleProxy from './proxy';
-import handleRedirect from './redirect';
-import apiRouter from './router';
+const schemas = {
+	'/v1/chat/completions': z.object({
+		model: z.enum([
+			'gpt-3.5-turbo',
+			'gpt-4-1106-preview'
+		]),
+		temperature: z.literal(0.8),
+		top_p: z.literal(1),
+		frequency_penalty: z.literal(0),
+		presence_penalty: z.literal(0),
+		n: z.literal(1),
+		stream: z.literal(true),
+		messages: z.array(z.object({
+			role: z.enum(['user']),
+			content: z.string()
+		})),
+		user: z.undefined()
+	}),
+	'/v1/completions': z.object({
+		model: z.enum([
+			'text-davinci-003'
+		]),
+		max_tokens: z.literal(100),
+		temperature: z.literal(0.5),
+		frequency_penalty: z.literal(0.5),
+		presence_penalty: z.literal(0.6),
+		stop: z.array(z.string()),
+		prompt: z.string(),
+		n: z.number(),
+		best_of: z.number(),
+		user: z.string()
+	})
+} as const;
 
-// Export a default object containing event handlers
+const ALLOWED_PATHS = [
+	'/v1/chat/completions',
+	'/v1/completions',
+	'/v1/audio/transcriptions'
+];
+
+const OPENAI_BASE = 'https://api.openai.com/';
+
+interface JWTPayload {
+	p: string[] // allowed paths
+	m: string // allowed models
+}
+
 export default {
-	// The fetch handler is invoked when this worker receives a HTTP(S) request
-	// and should return a Response (optionally wrapped in a Promise)
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
+		if (!env.OPENAI_TOKEN || !env.SHARED_JWT_SECRET) return new Response('Bad request: Missing environment variables', { status: 400 });
+
 		const url = new URL(request.url);
+		if (!ALLOWED_PATHS.includes(url.pathname)) return new Response('Bad request: Invalid path: ' + url.pathname, { status: 400 });
 
-		// You can get pretty far with simple logic like if/switch-statements
-		switch (url.pathname) {
-			case '/redirect':
-				return handleRedirect.fetch(request, env, ctx);
+		const tokenHeader = request.headers.get('Authorization');
+		if (!tokenHeader) return new Response('Bad request: Missing `Authorization` header', { status: 400 });
+		const token = tokenHeader.split(' ')[1];
+		if (!token) return new Response('Bad request: Missing token', { status: 400 });
 
-			case '/proxy':
-				return handleProxy.fetch(request, env, ctx);
+		const isValid = await jwt.verify(token, env.SHARED_JWT_SECRET);
+		if (!isValid) return new Response('Bad request: Invalid token', { status: 400 });
+
+		const { payload } = jwt.decode<JWTPayload>(token);
+		const sub = payload?.sub;
+		if (!sub) return new Response('Bad request: Invalid user', { status: 400 });
+		// @TODO - check allowed paths from payload.p
+
+		const proxyUrl = OPENAI_BASE + (url.href.substring(url.origin.length + 1));
+
+		const newRequest = new Request(request);
+		newRequest.headers.set('Authorization', `Bearer ${env.OPENAI_TOKEN}`);
+
+		// @TODO - validate request body for /v1/audio/transcriptions
+		if (url.pathname === '/v1/chat/completions' || url.pathname === '/v1/completions') {
+			const schema = schemas[url.pathname];
+			const r = request.clone()
+			const json = await r.json();
+			const parsed = schema.parse(json)
+			// @TODO - use refinements in zod to add user id to request body
 		}
 
-		if (url.pathname.startsWith('/api/')) {
-			// You can also use more robust routing
-			return apiRouter.handle(request);
-		}
-
-		return new Response(
-			`Try making requests to:
-      <ul>
-      <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
-      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
-      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
-			{ headers: { 'Content-Type': 'text/html' } }
-		);
-	},
+		return fetch(proxyUrl, newRequest);
+	}
 };
